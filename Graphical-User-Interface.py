aea49b339c9c0548bb83a90f42a705894778ac53
mhamadjaafarr <mhamadjaaafarr@gmail.com>
2024-07-28 13:17:45 +0300

    GraphApp class inherits from tk.Tk and represents the main application window     for the social media network graph application

--- Graphical-User-Interface.py
+++ Graphical-User-Interface.py
@@ -7,3 +7,11 @@ import networkx as nx
 import matplotlib.pyplot as plt  
 from Graph import AdjacencyMatrix  # Importing the AdjacencyMatrix class from the Graph module.
 import os
+
+
+class GraphApp(tk.Tk):
+    """
+    GraphApp class inherits from tk.Tk and represents the main application window
+    for the social media network graph application.
+
+    """
